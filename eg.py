c9589e87517abcc7dd861e3e3d35b3586a4e1b80
kavin-uzumaki <132762076+kavin-uzumaki@users.noreply.github.com>
2025-01-24 20:43:50 +0530

    works018

---
+++ eg.py
@@ -0,0 +1,13 @@
+import requests
+headers = {
+    "Content-Type": "application/json",
+    "X-Master-Key": "$2a$10$zCxD0eePhaxSpav1iZtzzO41se.8HoND.wMVD5IYqeQpGX3QqYfai"
+}
+
+idURL = "https://api.jsonbin.io/v3/b/679255e6e41b4d34e47d86da"
+
+upreq = requests.get(idURL, headers= headers).json()["record"]
+# upreq["rc"].append(473)
+ele = upreq["rc"].pop(0)
+print(ele)
+upreq2 = requests.put(idURL, headers= headers, json = upreq)
